9087eb394cddab30b10a8ded641d599f8230026a
Leonardo Silveira <sombriks@gmail.com>
2017-04-09 19:25:22 -0300

    still broken

--- trabalho-01-missionarios-e-canibais/index.js
+++ trabalho-01-missionarios-e-canibais/index.js
@@ -5,6 +5,10 @@ const solucao = []
 
 const eh_solucao = (s) => s.m2.m == 3
 
+const eh_valido = (s) => s.m1.m >= s.m1.c && s.m2.m >= s.m2.c ||
+  (s.m1.m == 0 && s.m2.m >= s.m2.c) ||
+  (s.m2.m == 0 && s.m1.m >= s.m1.c)
+
 const busca_solucao = (estado) => {
 
   solucao.push(estado)
@@ -12,30 +16,38 @@ const busca_solucao = (estado) => {
   if (eh_solucao(estado)) {
     console.log("Solucao encontrada:")
     console.log(solucao)
-    proccess.exit(0)
+    process.exit(0)
   }
 
   estado.seguintes = cria_estados_validos(estado)
   estado.seguintes.map(e => busca_solucao(e))
+
+  if (!eh_solucao(estado))
+    solucao.pop()
 }
 
 const cria_estados_validos = (s) => {
   let estados = []
-  pode_mm_m(s) && estados.push(mm_m(s))
-  pode_mm_c(s) && estados.push(mm_c(s))
-  pode_mc_m(s) && estados.push(mc_m(s))
-  pode_mc_c(s) && estados.push(mc_c(s))
-  pode_cc_m(s) && estados.push(cc_m(s))
-  pode_cc_c(s) && estados.push(cc_c(s))
+  let novo = mm_m(s)
+  if (eh_valido(novo))
+    estados.push(novo)
+  novo = mm_c(s)
+  if (eh_valido(novo))
+    estados.push(novo)
+  novo = mc_m(s)
+  if (eh_valido(novo))
+    estados.push(novo)
+  novo = mc_c(s)
+  if (eh_valido(novo))
+    estados.push(novo)
+  novo = cc_m(s)
+  if (eh_valido(novo))
+    estados.push(novo)
+  novo = cc_c(s)
+  if (eh_valido(novo))
+    estados.push(novo)
   return estados
 }
-
-const pode_mm_m = (s) => s.m1.m - 2 >= s.m1.c
-const pode_mm_c = (s) => s.m1.m - 2 > s.m1.c
-const pode_mc_m = (s) => s.m2.m > s.m2.c
-const pode_mc_c = (s) => s.m1.m > s.m1.c
-const pode_cc_m = (s) => s.m2.m - 1 > s.m2.c
-const pode_cc_c = (s) => s.m2.m >= s.m2.c + 2
 
 const mm_m = (s) => {
   let novo = JSON.parse(JSON.stringify(s))
@@ -83,5 +95,3 @@ const cc_c = (s) => {
 }
 
 busca_solucao(estado_inicial)
-
-console.log(solucao)
