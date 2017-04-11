a42ddcf27a84486e7196c0949098d67dd1e5626e
Leonardo Silveira <sombriks@gmail.com>
2017-04-11 01:44:32 -0300

    estepresta edition

--- trabalho-01-missionarios-e-canibais/index.js
+++ trabalho-01-missionarios-e-canibais/index.js
@@ -3,55 +3,37 @@ const estado_inicial = { m1: { m: 3, c: 3 }, m2: { m: 0, c: 0 }, b: 'e' }
 
 const solucao = []
 
-const eh_solucao = (s) => s.m2.m == 3
+const eh_solucao = (s) => s.m2.m == 3 && s.m2.c == 3
 
-// XXX otimizar
 const eh_valido = (s) => {
   let valido = true
-  if (s.m1.m != 0 && s.m1.m < s.m1.c)
-    valido = false
-  if (s.m2.m != 0 && s.m2.m < s.m2.c)
-    valido = false
-  if (s.m1.m > 3)
-    valido = false
-  if (s.m1.c > 3)
-    valido = false
-  if (s.m2.m > 3)
-    valido = false
-  if (s.m2.c > 3)
-    valido = false
-  if (s.m1.m < 0)
-    valido = false
-  if (s.m1.c < 0)
-    valido = false
-  if (s.m2.m < 0)
-    valido = false
-  if (s.m2.c < 0)
+  if ((s.m1.m != 0 && s.m1.m < s.m1.c) || (s.m2.m != 0 && s.m2.m < s.m2.c) ||
+    (s.m1.m > 3) || (s.m1.c > 3) || (s.m2.m > 3) || (s.m2.c > 3) || (s.m1.m < 0) ||
+    (s.m1.c < 0) || (s.m2.m < 0) || (s.m2.c < 0))
     valido = false
   return valido
 }
-
-const busca_solucao = (estado) => {
-
-  solucao.push(estado)
-
-  console.log("parcial:")
-  console.log(solucao)
-
-  if (eh_solucao(estado)) {
-    console.log("Solucao encontrada:")
-    console.log(solucao)
-    process.exit(0)
-  }
-
-  // TODO não precisamos dos seguintes aqui
-  // falta verificar se os validos criados não contém algum estado pai
-  estado.seguintes = cria_estados_validos(estado)
-  estado.seguintes.map(e => busca_solucao(e))
-
-  if (!eh_solucao(estado))
-    solucao.pop()
+const move = (s, v1m, v2m, v1c, v2c, b) => {
+  let n = JSON.parse(JSON.stringify(s))
+  n.m1.m += v1m
+  n.m2.m += v2m
+  n.m1.c += v1c
+  n.m2.c += v2c
+  n.b = b
+  return n
 }
+
+const e_mm = (s) => move(s, -2, 2, 0, 0, 'd')
+
+const e_cc = (s) => move(s, 0, 0, -2, 2, 'd')
+
+const e_mc = (s) => move(s, -1, 1, -1, 1, 'd')
+
+const d_m = (s) => move(s, 1, -1, 0, 0, 'e')
+
+const d_c = (s) => move(s, 0, 0, 1, -1, 'e')
+
+const d_mc = (s) => move(s,1, -1, 1, -1, 'e')
 
 const cria_estados_validos = (s) => {
   let estados = []
@@ -64,55 +46,35 @@ const cria_estados_validos = (s) => {
     novo = e_mc(s)
     if (eh_valido(novo)) estados.push(novo)
   } else {
-    // barco na direita, traz um
+    // barco na direita, traz um ou dois
     let novo = d_m(s)
     if (eh_valido(novo)) estados.push(novo)
     novo = d_c(s)
+    if (eh_valido(novo)) estados.push(novo)
+    novo = d_mc(s)
     if (eh_valido(novo)) estados.push(novo)
   }
   return estados
 }
 
-const e_mm = (s) => {
-  let n = JSON.parse(JSON.stringify(s))
-  n.m1.m -= 2
-  n.m2.m += 2
-  n.b = 'd'
-  return n
-}
+const solucao_tem = (e) => solucao.filter(s => JSON.stringify(s) == JSON.stringify(e)).length > 0
 
-const e_cc = (s) => {
-  let n = JSON.parse(JSON.stringify(s))
-  n.m1.c -= 2
-  n.m2.c += 2
-  n.b = 'd'
-  return n
-}
+const busca_solucao = (estado) => {
 
-const e_mc = (s) => {
-  let n = JSON.parse(JSON.stringify(s))
-  n.m1.m -= 1
-  n.m2.m += 1
-  n.m1.c -= 1
-  n.m2.c += 1
-  n.b = 'd'
-  return n
-}
+  solucao.push(estado)
 
-const d_m = (s) => {
-  let n = JSON.parse(JSON.stringify(s))
-  n.m2.m -= 1
-  n.m1.m += 1
-  n.b = 'e'
-  return n
-}
+  if (eh_solucao(estado)) {
+    console.log("Solucao encontrada:")
+    console.log(solucao)
+    process.exit(0)
+  }
 
-const d_c = (s) => {
-  let n = JSON.parse(JSON.stringify(s))
-  n.m2.c -= 1
-  n.m1.c += 1
-  n.b = 'e'
-  return n
+  let seguintes = cria_estados_validos(estado)
+  seguintes.map(e => busca_solucao(e))
+
+  if (!eh_solucao(estado)) {
+    solucao.pop()
+  }
 }
 
 busca_solucao(estado_inicial)
