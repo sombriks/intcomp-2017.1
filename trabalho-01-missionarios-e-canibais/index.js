cb7fd2c81109685cf08f5d1895292e1b488bffbe
Leonardo Silveira <sombriks@gmail.com>
2017-04-11 01:53:26 -0300

    ta bonito edition

--- trabalho-01-missionarios-e-canibais/index.js
+++ trabalho-01-missionarios-e-canibais/index.js
@@ -1,18 +1,19 @@
 
+const solucao = [] // array de estados com o caminho de solucoes
+
 const estado_inicial = { m1: { m: 3, c: 3 }, m2: { m: 0, c: 0 }, b: 'e' }
 
-const solucao = []
-
-const eh_solucao = (s) => s.m2.m == 3 && s.m2.c == 3
+const eh_solucao = (s) => s.m2.m == 3 && s.m2.c == 3 // pra saber se resolveu
 
 const eh_valido = (s) => {
   let valido = true
   if ((s.m1.m != 0 && s.m1.m < s.m1.c) || (s.m2.m != 0 && s.m2.m < s.m2.c) ||
-    (s.m1.m > 3) || (s.m1.c > 3) || (s.m2.m > 3) || (s.m2.c > 3) || (s.m1.m < 0) ||
-    (s.m1.c < 0) || (s.m2.m < 0) || (s.m2.c < 0))
-    valido = false
+    (s.m1.m > 3) || (s.m1.c > 3) || (s.m2.m > 3) || (s.m2.c > 3) || 
+    (s.m1.m < 0) || (s.m1.c < 0) || (s.m2.m < 0) || (s.m2.c < 0))
+    valido = false // todos os casos invalidos
   return valido
 }
+
 const move = (s, v1m, v2m, v1c, v2c, b) => {
   let n = JSON.parse(JSON.stringify(s))
   n.m1.m += v1m
@@ -20,45 +21,32 @@ const move = (s, v1m, v2m, v1c, v2c, b) => {
   n.m1.c += v1c
   n.m2.c += v2c
   n.b = b
-  return n
+  return n // regra generica de movimento entre as margens
 }
 
-const e_mm = (s) => move(s, -2, 2, 0, 0, 'd')
-
-const e_cc = (s) => move(s, 0, 0, -2, 2, 'd')
-
-const e_mc = (s) => move(s, -1, 1, -1, 1, 'd')
-
-const d_m = (s) => move(s, 1, -1, 0, 0, 'e')
-
-const d_c = (s) => move(s, 0, 0, 1, -1, 'e')
-
-const d_mc = (s) => move(s,1, -1, 1, -1, 'e')
-
 const cria_estados_validos = (s) => {
-  let estados = []
+  let estados = [] // aqui a inteligencia pra criar estados a partir de um recebido
   if (s.b == 'e') {
     // barco na esquerda, leva dois
-    let novo = e_mm(s)
+    let novo = move(s, -2, 2, 0, 0, 'd')
     if (eh_valido(novo)) estados.push(novo)
-    novo = e_cc(s)
+    novo = move(s, 0, 0, -2, 2, 'd')
     if (eh_valido(novo)) estados.push(novo)
-    novo = e_mc(s)
+    novo = move(s, -1, 1, -1, 1, 'd')
     if (eh_valido(novo)) estados.push(novo)
   } else {
     // barco na direita, traz um ou dois
-    let novo = d_m(s)
+    let novo = move(s, 1, -1, 0, 0, 'e')
     if (eh_valido(novo)) estados.push(novo)
-    novo = d_c(s)
+    novo = move(s, 0, 0, 1, -1, 'e')
     if (eh_valido(novo)) estados.push(novo)
-    novo = d_mc(s)
+    novo = move(s,1, -1, 1, -1, 'e')
     if (eh_valido(novo)) estados.push(novo)
   }
   return estados
 }
 
-const solucao_tem = (e) => solucao.filter(s => JSON.stringify(s) == JSON.stringify(e)).length > 0
-
+// hora de ser recursivo
 const busca_solucao = (estado) => {
 
   solucao.push(estado)
@@ -66,15 +54,15 @@ const busca_solucao = (estado) => {
   if (eh_solucao(estado)) {
     console.log("Solucao encontrada:")
     console.log(solucao)
-    process.exit(0)
+    process.exit(0) // aborta pra findar a recursao
   }
+  
+  // para cada estado possivel a partir do atual, buscar solucao para eles
+  cria_estados_validos(estado).map(e => busca_solucao(e))
 
-  let seguintes = cria_estados_validos(estado)
-  seguintes.map(e => busca_solucao(e))
-
-  if (!eh_solucao(estado)) {
+  if (!eh_solucao(estado)) 
     solucao.pop()
-  }
 }
 
+// bootstrap aqui mesmo
 busca_solucao(estado_inicial)
